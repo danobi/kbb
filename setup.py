1a72964a779bd5c4a480de5aa078b50b6015cd77
Daniel Xu <dlxu2@yahoo.com>
2016-04-05 01:21:33 -0500

    Add initial setup.py config file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='KanBanBoard',
+      version='0.1',
+      description='kbb is a TUI based Kanban board with Google Tasks integration',
+      author='Daniel Xu',
+      author_email='dlxu2@yahoo.com',
+      url='https://github.com/danobi/kbb',
+     )
