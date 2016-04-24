e4371cde99b646b09dc3a735e81463329e996d23
Daniel Xu <dlxu2@yahoo.com>
2016-04-24 16:49:31 -0500

    Add test for two way online task move

--- tests/test_core.py
+++ tests/test_core.py
@@ -123,3 +123,43 @@ def test_move_task_online_one_way():
     k.delete_task(new_task_id)
     assert new_len - old_len == 1
 
+
+def test_move_task_online_two_way():
+    k = kbb.Kbb()
+
+    task_name = 'online move two way task test'
+
+    old_len = len(k._get_all_cloud_tasks())
+    task = k.new_task(task_name)
+    new_len = len(k._get_all_cloud_tasks())
+
+    # find new task id
+    new_task_id = None
+    for t in k.get_task_list():
+        if t.title == task_name:
+            new_task_id = t.task_id
+
+    if not new_task_id:
+        assert False
+
+    # move task to done
+    k.move_task(new_task_id, k.get_stage_names()[-1])
+    assert k._locate_task(new_task_id).stage == k.get_stage_names()[-1]
+    assert k._locate_task(new_task_id).status == Task.DONE
+
+    
+    # move task to not done
+    k.move_task(new_task_id, k.get_stage_names()[0])
+
+    # find new task id
+    new_task_id = None
+    for t in k.get_task_list():
+        if t.title == task_name:
+            new_task_id = t.task_id
+
+    assert k._locate_task(new_task_id).stage == k.get_stage_names()[0]
+    assert k._locate_task(new_task_id).status == Task.NOTDONE
+
+    # clean up
+    k.delete_task(new_task_id)
+    assert new_len - old_len == 1
